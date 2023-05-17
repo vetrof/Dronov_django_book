25fa66344bacfdfaf3c0f85504578c774c99e4a8
Vitaly Vetrof <vetrof@gmail.com>
2023-05-17 22:26:34 +0600

    update

--- project_1/bboard/views.py
+++ project_1/bboard/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from bboard.models import Bd
+
 
 def index_view(request):
-    return HttpResponse('hello world')
+    s = 'список обьявлений\r\n\r\n\r\n'
+    for bd in Bd.objects.order_by('-published'):
+        s += f'{bd.title} : {bd.content} : {bd.price} \r\n\r\n'
+
+    return HttpResponse(s, content_type='text/plain; charset=utf-8')
