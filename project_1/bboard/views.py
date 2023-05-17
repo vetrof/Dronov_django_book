e853e0578c3599d14289eddd9292bb6788c735d8
Vitaly Vetrof <vetrof@gmail.com>
2023-05-17 23:00:48 +0600

    update

--- project_1/bboard/views.py
+++ project_1/bboard/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 from bboard.models import Bd
 
 
 def index_view(request):
-    s = 'список обьявлений\r\n\r\n\r\n'
-    for bd in Bd.objects.order_by('-published'):
-        s += f'{bd.title} : {bd.content} : {bd.price} \r\n\r\n'
-
-    return HttpResponse(s, content_type='text/plain; charset=utf-8')
+    template = loader.get_template('bboard/index.html')
+    bbs = Bd.objects.order_by('-published')
+    context = {'bbs': bbs}
+    return HttpResponse(template.render(context, request))
